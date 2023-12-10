ed4b868638c6935272d2ade9cd3ddb913160b58a
dariusur <darius.u32@gmail.com>
2023-12-10 17:36:48 +0200

    Update file

--- MAX7219_Arduino/example/fill_screen/fill_screen.ino
+++ MAX7219_Arduino/example/fill_screen/fill_screen.ino
@@ -5,7 +5,7 @@
 * When the intensity reaches maximum new loop begins.
 */
 
-#include <MAX7219.h>
+#include "MAX7219.h"
 
 const uint8_t DIN = 5, CS = 6, CLK = 7; // DIN, CS, CLK pins of MAX7219 module
 
@@ -17,7 +17,7 @@ void setup(){
 
 void loop(){
   for(uint8_t intensity=0;intensity<=15;intensity++){
-    matrix.configIntensity(intensity);
+    matrix.sendData(max7219_reg_intensity, intensity);
     for(uint8_t i=0;i<8;i++){
       for(uint8_t j=0;j<8;j++){
         matrix.switchLED(i,j,1);
